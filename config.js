5a328721c6482bfcf945f702ae50da16cabe6381
vamsibadugu-lab <vamsibadugu@gmail.com>
2026-05-25 09:21:27 -0500

    Update config.js

--- config.js
+++ config.js
@@ -7,5 +7,5 @@
 
 export const CONFIG = {
   SUPABASE_URL: "https://mkbqhstobcllexyijklc.supabase.co",
-  SUPABASE_ANON_KEY: "sb_publishable_Nb96Gv3aGGYvvhZmyrqcfQ_Xz7Z_zAa"
+  SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1rYnFoc3RvYmNsbGV4eWlqa2xjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzk3MDk4MDQsImV4cCI6MjA5NTI4NTgwNH0.RXx5kPPXZ3ubm_AuOpPBCu3SEe2_-_L80JWyGSF3lyc"
 };
